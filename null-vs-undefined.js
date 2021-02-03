9a47752554f4fa85c5c6a754ca771eeb83a4544d
Abu Naser Dipu <abunaserdipu0@gmail.com>
2021-02-03 09:48:55 +0600

    practiced null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,30 @@
+//undefined1
+let sir;
+console.log(sir);
+
+//undefined2
+function add(num1, num2){
+    const sum = num1 + num2;
+    return
+}
+const result = add(3, 4);
+console.log(result);
+
+//undefined3
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const sum = add(13);
+console.log(sum);
+
+//undefined4
+let fun = undefined;
+console.log(fun);
+
+//undefined5
+const sir = {name: "Ahmed Karim", age: 45, phone: "01192599995"};
+console.log(sir.address);
+
+//undefined6
+const number = [3, 5, 7];
+console.log(number[9]);
